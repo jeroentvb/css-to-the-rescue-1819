0228ad2d0f54638060628a12ce7cb89271c5cb4c
jeroentvb <jeroentvb@hotmail.com>
2019-02-26 14:39:08 +0100

    I should commit way more often. Lost track of changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,20 @@ const button = {
 }
 let root = document.documentElement
 
-button.dark.addEventListener('click', e => {
-  root.style.setProperty('--background-color', '#000')
-  root.style.setProperty('--text-color', '#fff')
-  root.style.setProperty('--text-inverted', '#000')
+button.light.addEventListener('click', e => {
+  root.style.setProperty('--invert', 'invert(0%)')
+  root.style.setProperty('--invert-color', '#fff')
+  // root.style.setProperty('--background-color', '#000')
+  // root.style.setProperty('--text-color', '#fff')
+  // root.style.setProperty('--text-inverted', '#000')
+  // root.style.setProperty('--gradient', 'linear-gradient(to bottom, #2422aa 0%,#6b0042 100%)')
 })
 
-button.light.addEventListener('click', e => {
-  root.style.setProperty('--background-color', '#fff')
-  root.style.setProperty('--text-color', '#000')
-  root.style.setProperty('--text-inverted', '#fff')
+button.dark.addEventListener('click', e => {
+  root.style.setProperty('--invert', 'invert(100%)')
+  root.style.setProperty('--invert-color', '#000')
+  // root.style.setProperty('--background-color', '#fff')
+  // root.style.setProperty('--text-color', '#000')
+  // root.style.setProperty('--text-inverted', '#fff')
+  // root.style.setProperty('--gradient', 'linear-gradient(to bottom, #29d1b2 0%,#00c3ff 100%)')
 })
