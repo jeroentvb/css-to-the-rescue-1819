6c556a5b2c54374386626703dd8a27f7231125aa
jeroentvb <jeroentvb@hotmail.com>
2019-02-28 14:28:00 +0100

    Added more animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,17 +9,9 @@ let root = document.documentElement
 button.light.addEventListener('click', e => {
   root.style.setProperty('--invert', 'invert(0%)')
   root.style.setProperty('--invert-color', '#fff')
-  // root.style.setProperty('--background-color', '#000')
-  // root.style.setProperty('--text-color', '#fff')
-  // root.style.setProperty('--text-inverted', '#000')
-  // root.style.setProperty('--gradient', 'linear-gradient(to bottom, #2422aa 0%,#6b0042 100%)')
 })
 
 button.dark.addEventListener('click', e => {
   root.style.setProperty('--invert', 'invert(100%)')
   root.style.setProperty('--invert-color', '#000')
-  // root.style.setProperty('--background-color', '#fff')
-  // root.style.setProperty('--text-color', '#000')
-  // root.style.setProperty('--text-inverted', '#fff')
-  // root.style.setProperty('--gradient', 'linear-gradient(to bottom, #29d1b2 0%,#00c3ff 100%)')
 })
